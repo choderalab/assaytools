f8d044a76792cab4645d0faccc8ab6ff8acbd8d3
John Chodera (MSKCC) <choderaj@mskcc.org>
2015-01-18 21:57:13 -0500

    Fixes to __init__.py

--- AssayTools/__init__.py
+++ AssayTools/__init__.py
@@ -1,7 +1,9 @@
 """AssayTools is a python library that allows users to model automated assays and analyze assay data using powerful Bayesian techniques that allow complete characterization of the uncertainty in fit models.
 """
 
-from assaytools import models
+from assaytools import platereader
+from assaytools import pymcmodels
+from assaytools import bindingmodels
 
 def test(label='full', verbose=2):
     """Run tests for mdtraj using nose.
@@ -14,6 +16,8 @@ def test(label='full', verbose=2):
     verbose : int, optional
         Verbosity value for test outputs, in the range 1-10. Default is 2.
     """
+    raise Exception("Eventually, this will test the package, but this has not currently been implemented.")
+
     import assaytools
     from assaytools.testing.nosetester import AssaytoolsTester
     tester = AssaytoolsTester(assaytools)
@@ -21,13 +25,3 @@ def test(label='full', verbose=2):
 # prevent nose from discovering this function, or otherwise when its run
 # the test suite in an infinite loop
 test.__test__ = False
-
-
-def capi():
-    import os
-    import sys
-    module_path = sys.modules['assaytools'].__path__[0]
-    return {
-        'lib_dir':  os.path.join(module_path, 'core', 'lib'),
-        'include_dir': os.path.join(module_path, 'core', 'lib'),
-    }
