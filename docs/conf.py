21c831c9c00de86ee82b53892f45e1f263b5a261
John Chodera (MSKCC) <choderaj@mskcc.org>
2015-02-08 22:16:38 -0500

    Switched to sphinx_rtd_theme.

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,13 @@ import os
 
 import assaytools
 import assaytools.version
-import msmb_theme
+
+# Use mock to make our code think that numpy and scipy are available, even though they might not be available on readthedocs
+import mock
+
+MOCK_MODULES = ['numpy', 'scipy', "numpy.linalg", "scipy.special", "scipy.stats"]
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -128,7 +134,13 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "msmb_theme"
+#html_theme = "default"
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -137,7 +149,6 @@ html_theme = "msmb_theme"
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
-html_theme_path = [msmb_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
