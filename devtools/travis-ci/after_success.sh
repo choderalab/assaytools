56182d2385465f21c0e64a2fa453f6a00d85cd5e
John Chodera (MSKCC) <choderaj@mskcc.org>
2015-10-07 13:31:47 -0400

    More updates to travis

--- devtools/travis-ci/after_success.sh
+++ devtools/travis-ci/after_success.sh
@@ -1,40 +1,36 @@
-# Coveralls is temporarily commented out until we can set this up.
-#coveralls
+#!/bin/bash
+# Must be invoked with $PACKAGENAME
 
 echo $TRAVIS_PULL_REQUEST $TRAVIS_BRANCH
+PUSH_DOCS_TO_S3=false
 
-if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+if [ "$TRAVIS_PULL_REQUEST" = true ]; then
     echo "This is a pull request. No deployment will be done."; exit 0
 fi
 
 
-if [[ "$TRAVIS_BRANCH" != "master" ]]; then
+if [ "$TRAVIS_BRANCH" != "master" ]; then
     echo "No deployment on BRANCH='$TRAVIS_BRANCH'"; exit 0
 fi
 
 
-if [[ "2.7 3.3 3.4" =~ "$python" ]]; then
-    echo "Uploading to binstar..."
-    conda install --yes binstar
-    binstar -t $BINSTAR_TOKEN upload --force -u omnia -p assaytools-dev $HOME/miniconda/conda-bld/linux-64/assaytools-dev-*
+# Deploy to binstar
+conda install --yes anaconda-client jinja2
+binstar -t $BINSTAR_TOKEN upload --force -u omnia -p ${PACKAGENAME}-dev $HOME/miniconda/conda-bld/*/${PACKAGENAME}-dev-*.tar.bz2
+
+if [ $PUSH_DOCS_TO_S3 = true ]; then
+   # Create the docs and push them to S3
+   # -----------------------------------
+    conda install --yes pip
+    conda config --add channels http://conda.binstar.org/omnia
+    conda install --yes `conda build devtools/conda-recipe --output`
+    pip install numpydoc s3cmd msmb_theme
+    conda install --yes `cat docs/requirements.txt | xargs`
+
+    conda list -e
+
+    (cd docs && make html && cd -)
+    ls -lt docs/_build
+    pwd
+    python devtools/ci/push-docs-to-s3.py
 fi
-
-if [[ "$python" != "2.7" ]]; then
-    echo "No deploy on PYTHON_VERSION=${python}"; exit 0
-fi
-
-
-# Create the docs and push them to S3
-# -----------------------------------
-
-conda install --yes pip
-conda config --add channels http://conda.binstar.org/omnia
-conda install --yes `conda build devtools/conda-recipe --output`
-pip install numpydoc s3cmd msmb_theme
-conda install --yes `cat docs/requirements.txt | xargs`
-
-conda list -e
-
-(cd docs && make html)
-#python devtools/ci/push-docs-to-s3.py
-#python devtools/ci/update-versions.py
