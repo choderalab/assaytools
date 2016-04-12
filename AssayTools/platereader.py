47e3a796d7eeb41d01240e5fe7e7a60c84a0781e
sonyahanson <hansons@mskcc.org>
2016-04-12 11:42:21 -0400

    removed references to pint

--- AssayTools/platereader.py
+++ AssayTools/platereader.py
@@ -11,7 +11,6 @@ Tools for assisting in reading and manipulating data from plate readers.
 
 import numpy as np
 import re
-import pint
 from lxml import etree
 
 #=============================================================================================
