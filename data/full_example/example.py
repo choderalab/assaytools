2f73b68463cb6468492d97760faf2af094609c90
John Chodera (MSKCC) <choderaj@mskcc.org>
2016-04-13 00:20:41 -0400

    Finished prototype data model

--- data/full_example/example.py
+++ data/full_example/example.py
@@ -4,6 +4,32 @@ from autoprotocol.unit import Unit
 import numpy as np
 
 #
+# Define assay plate container
+#
+
+# Define the container type for 4titude 4ti-0223.
+# info: http://4ti.co.uk/microplates/black-clear-bottom/96-well/
+# drawing: http://4ti.co.uk/files/1614/0542/7662/4ti-0223_Marketing_Drawing.pdf
+# All arguments to ContainerType are required!
+capabilities = ['pipette', 'spin', 'absorbance', 'fluorescence', 'luminescence', 'incubate', 'gel_separate', 'cover', 'seal', 'stamp', 'dispense']
+well_diameter = Unit(6.30, "millimeters")
+well_area = np.pi * (well_diameter/2)**2
+container_type = ContainerType(name='4titude 4ti-0223', is_tube=False, well_count=96, well_depth_mm=Unit(11.15, 'millimeter'), well_volume_ul=Unit(300, 'milliliter'), well_coating='polystyrene', sterile=False, capabilities=capabilities, shortname='4ti-0223', col_count=12, dead_volume_ul=Unit(20,'milliliter'), safe_min_volume_ul=Unit(50, 'milliliter'))
+
+# Generate a unique container ID
+import uuid
+id = str(uuid.uuid4())
+
+# Define the container
+container = Container(name="assay-plate", id=id, container_type=container_type)
+
+# Initialize well properties.
+for well in container.all_wells():
+    well.set_volume(Unit(0.0, 'microliters')) # well starts empty
+    well.set_properties({'contents' : dict()}) # contains the contents
+    well.set_properties({'area' : well_area})
+
+#
 # Load DMSO stocks
 #
 
@@ -31,6 +57,14 @@ class Solution(object):
         self.name = name
         self.concentration = concentration
         self.uncertainty = uncertainty
+
+class BufferSolution(Solution):
+    """A pure buffer solution.
+    """
+    def __init__(self, name):
+        self.name = name
+        self.concentration = 0.0
+        self.uncertainty = 0.0
 
 class ProteinSolution(Solution):
     """A protein solution prepared spectrophotometrically.
@@ -78,87 +112,103 @@ class DMSOStockSolution(Solution):
 
 solutions = dict()
 solutions['Abl'] = ProteinSolution(name='1 uM Abl', absorbance=4.24, extinction_coefficient=49850, molecular_weight=41293.2, ul_protein_stock=165.8, ml_buffer=14.0)
+solutions['Abl'] = BufferSolution('20 mM Tris buffer')
 solutions['BOS'] = DMSOStockSolution(dmso_stocks['BOS001'])
 solutions['BSI'] = DMSOStockSolution(dmso_stocks['BOI001'])
 solutions['GEF'] = DMSOStockSolution(dmso_stocks['GEF001'])
 solutions['ERL'] = DMSOStockSolution(dmso_stocks['ERL001'])
 
 #
+# Dispense protein solution
+#
+
+assay_volume = Unit(100.0, 'microliters')
+for well in container.all_wells():
+    well_name = well.humanize()
+    contents = well.properties['contents']
+    if well_name[0] in ['A', 'C', 'E', 'G']:
+        contents['Abl'] = assay_volume
+    else:
+        contents['buffer'] = assay_volume
+    well.set_volume(well.volume + assay_volume)
+
+#
 # Read ligand concentrations
 #
 
+d300_xml_filename = 'Src_Bos_Ima_96well_Mar2015 2015-03-07 1736.DATA.xml'
 
+# Read HP D300 XML file
+import xml.etree.ElementTree as ET
+tree = ET.parse(d300_xml_filename)
+root = tree.getroot()
+
+# Read fluids
+fluids = root.findall('./Fluids/Fluid')
+
+# TODO: Rewrite fluid names to match stock names.
+fluids[0].attrib['Name'] = 'BOS001'
+fluids[1].attrib['Name'] = 'IMA001'
+fluids[2].attrib['Name'] = 'DMSO'
+
+def humanize_d300_well(row, column):
+    """
+    Return the humanized version of a D300 well index.
+    """
+    return chr(row + 97) + str(column+1)
+
+# Read dispensed volumes
+dispensed = root.findall('./Dispensed')[0]
+volume_unit = dispensed.attrib['VolumeUnit'] # dispensed volume unit
+wells = dispensed.findall('Plate/Well')
+for well in wells:
+    row = int(well.attrib['R'])
+    column = int(well.attrib['C'])
+    dispensed_fluids = well.findall('Fluid')
+    for dispensed_fluid in dispensed_fluids:
+        dispensed_fluid_index = int(dispensed_fluid.attrib['Index'])
+        fluid_name = fluids[dispensed_fluid_index].attrib['Name']
+        total_volume = Unit(float(dispensed_fluid.attrib['TotalVolume']), volume_unit)
+        detail = dispensed_fluid.findall('Detail')
+        # Gather details
+        time = detail[0].attrib['Time']
+        cassette = int(detail[0].attrib['Cassette'])
+        head = int(detail[0].attrib['Head'])
+        dispensed_volume = Unit(float(detail[0].attrib['Volume']), volume_unit)
+
+        # Retrieve well from container.
+        well_name = humanize_d300_well(row, column)
+        well = container.well(well_name)
+        contents = well.properties['contents']
+        contents[fluid_name] = dispensed_volume
+        well.set_volume(well.volume + dispensed_volume)
 
 #
-# Define assay plate container
+# Load plate reader data.
 #
 
-# Define the container type for 4titude 4ti-0223.
-# info: http://4ti.co.uk/microplates/black-clear-bottom/96-well/
-# drawing: http://4ti.co.uk/files/1614/0542/7662/4ti-0223_Marketing_Drawing.pdf
-# All arguments to ContainerType are required!
-capabilities = ['pipette', 'spin', 'absorbance', 'fluorescence', 'luminescence', 'incubate', 'gel_separate', 'cover', 'seal', 'stamp', 'dispense']
-well_diameter = Unit(6.30, "millimeters")
-well_area = np.pi * (well_diameter/2)**2
-container_type = ContainerType(name='4titude 4ti-0223', is_tube=False, well_count=96, well_depth_mm=Unit(11.15, 'millimeter'), well_volume_ul=Unit(300, 'milliliter'), well_coating='polystyrene', sterile=False, capabilities=capabilities, shortname='4ti-0223', col_count=12, dead_volume_ul=Unit(20,'milliliter'), safe_min_volume_ul=Unit(50, 'milliliter'))
-
-# Generate a unique container ID
-import uuid
-id = str(uuid.uuid4())
-
-# Define the container
-container = Container(name="assay-plate", id=id, container_type=container_type)
-
-#
-# Assay Parameters
-#
-
-Lstated = [ Unit(x, "moles/liter") for x in [20.0e-6,9.15e-6,4.18e-6,1.91e-6,0.875e-6,0.4e-6,0.183e-6,0.0837e-6,0.0383e-6,0.0175e-6,0.008e-6,0] ] # gefitinib concentration
-Pstated = Unit(0.5e-6, "moles/liter")
-well_volume = Unit(100, "milliliter")
-receptor_name = 'Src'
-ligand_name = 'gefitinib'
-
-# Load data into well format.
 filename = "Abl Gef gain 120 bw1020 2016-01-19 15-59-53_plate_1.xml"
 from assaytools import platereader
 data = platereader.read_icontrol_xml(filename)
 
 # Define wells for fluorescence assay (the verbose way; we'd provide convenience methods to format the plate)
-ncolumns = 12
-well_group = WellGroup([])
-for column in range(ncolumns):
-    for row in ['A', 'B']:
-        well_name = row + str(column+1)
-        well = container.well(well_name)
-        well.set_volume(well_volume)
-        well.set_properties({'area' : well_area})
+for well in container.all_wells():
+    well_name = well.humanize()
 
-        # Set concentrations of well components
-        if row == 'A':
-            well.set_properties({'concentrations' : {receptor_name : Pstated, ligand_name : Lstated[column]} })
-            well.set_properties({'concentration_uncertainties' : {receptor_name  : 0.10 * Pstated, ligand_name : 0.08 * Lstated[column]} })
-        elif row == 'B':
-            well.set_properties({'concentrations' : {ligand_name : Lstated[column]} })
-            well.set_properties({'concentration_uncertainties' : {ligand_name : 0.08 * Lstated[column]} })
-
-        # Attach plate reader data
-        for wavelength in ['280', '350', '480']:
-            well.set_properties({' absorbance_' + wavelength + 'nm' : data['Abs_' + wavelength]['well_data'][well_name] })
-        emission_wavelength = '480'
-        for excitation_wavelength in ['280', '350']:
-            well.set_properties({'fluorescence_top_ex' + excitation_wavelength + 'nm_em' + emission_wavelength + 'nm' : data[excitation_wavelength + '_TopRead']['well_data'][well_name]})
-            well.set_properties({'fluorescence_bottom_ex' + excitation_wavelength + 'nm_em' + emission_wavelength + 'nm' : data[excitation_wavelength + '_BottomRead']['well_data'][well_name]})
-
-        # Add to well group
-        well_group.append(well)
+    # Attach plate reader data
+    for wavelength in ['280', '350', '480']:
+        well.set_properties({' absorbance_' + wavelength + 'nm' : data['Abs_' + wavelength]['well_data'][well_name] })
+    emission_wavelength = '480'
+    for excitation_wavelength in ['280', '350']:
+        well.set_properties({'fluorescence_top_ex' + excitation_wavelength + 'nm_em' + emission_wavelength + 'nm' : data[excitation_wavelength + '_TopRead']['well_data'][well_name]})
+        well.set_properties({'fluorescence_bottom_ex' + excitation_wavelength + 'nm_em' + emission_wavelength + 'nm' : data[excitation_wavelength + '_BottomRead']['well_data'][well_name]})
 
 # TODO: Analyze the well group.
 # This is just a non-working stub for now.
 # Create a model
-from assaytools.analysis import CompetitiveBindingAnalysis
-model = CompetitiveBindingAnalysis(wells=well_group, receptor=receptor_name, ligands=[ligand_name])
+#from assaytools.analysis import CompetitiveBindingAnalysis
+#model = CompetitiveBindingAnalysis(wells=well_group, receptor=receptor_name, ligands=[ligand_name])
 # fit the maximum a posteriori (MAP) estimate
-map = model.map_fit()
+#map = model.map_fit()
 # run some MCMC sampling and return the MCMC object
-mcmc = model.run_mcmc()
+#mcmc = model.run_mcmc()
