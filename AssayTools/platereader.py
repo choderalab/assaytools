61aa676b365f2afa4a365e1abcb8119afe849f6d
sonyahanson <hansons@mskcc.org>
2016-04-15 13:07:06 -0400

    shifted row and columns functions out of read_icontrol_xml function

--- AssayTools/platereader.py
+++ AssayTools/platereader.py
@@ -17,32 +17,6 @@ from lxml import etree
 # Tecan Infinite plate reader helper functions
 #=============================================================================================
 
-def read_emission_spectra_text(filename):
-    """
-    Read text-formatted emission spectra.
-
-    Parameters
-    ----------
-    filename : str
-       The Tecan Infinite output filen to be read.
-
-    Returns
-    -------
-    SRC_280 : numpy.array
-    SRC_280_x : numpy.array
-    SRC_280_x_num : numpy.array
-
-    Examples
-    --------
-
-    """
-
-    SRC_280 = np.genfromtxt(filename, dtype='str')
-    SRC_280_x = SRC_280[0,:]
-    SRC_280_x_num = re.findall(r'\d+', str(SRC_280_x )[1:-1])
-
-    return [SRC_280, SRC_280_x, SRC_280_x_num]
-
 def read_icontrol_xml(filename):
     """
     Read a Tecan iControl XML-formatted file and return all section data.
@@ -106,92 +80,115 @@ def read_icontrol_xml(filename):
 
         return well_data
 
-    def identify_rows_and_cols(well_data):
-        """
-        Build a sorted list of unique rows and columns.
-
-        Parameters
-        ----------
-        well_data : dict
-           well_data[well_name] is the value associated with well_name (e.g. well_data['D7'] = 423)
-
-        Returns
-        -------
-        rows : list of str
-           Sorted list of unique rows.
-        cols : list of str
-           Sorted listof unique columns.
-
-        Examples
-        --------
-
-        >>> well_data = { 'A1' : 1, 'A2' : 2, 'A3' : 3, 'B1' : 4, 'B2' : 5, 'B3', 6 }
-        >>> [rows, cols] = identify_rows_and_cols(well_data)
-
-        """
-
-        rows = set()
-        cols = set()
-
-        for (well_name, value) in well_data.iteritems():
-            # Parse well names like 'A7' and 'D8'
-            m = re.match(r"([A-Z]+)([0-9]+)", well_name)
-
-            row = m.group(1)
-            col = int(m.group(2))
-
-            rows.add(row)
-            cols.add(col)
-
-        rows = sorted(list(rows))
-        cols = [ '%s' % entry for entry in sorted(list(cols)) ] # convert back to str after sorting numerically
-
-        return [rows, cols]
-
-    def extract_rows(well_data, rows, cols):
-        """
-        Examples
-        --------
-
-        >>> well_data = { 'A1' : 1, 'A2' : 2, 'A3' : 3, 'B1' : 4, 'B2' : 5, 'B3', 6 }
-        >>> [rows, cols] = identify_rows_and_cols(well_data)
-        >>> row_data = extract_rows(well_data, rows, cols)
-
-        """
-
-        row_data = dict()
-        for row in rows:
-            row_data[row] = np.zeros([len(cols)], np.float64)
-            for (index, col) in enumerate(cols):
-                row_data[row][index] = well_data[row + col]
-        return row_data
-
-    def extract_cols(well_data, rows, cols):
-        """
-        Examples
-        --------
-
-        >>> well_data = { 'A1' : 1, 'A2' : 2, 'A3' : 3, 'B1' : 4, 'B2' : 5, 'B3', 6 }
-        >>> [rows, cols] = identify_rows_and_cols(well_data)
-        >>> col_data = extract_cols(well_data, rows, cols)
-
-        """
-
-        col_data = dict()
-        for col in cols:
-            col_data[col] = np.zeros([len(rows)], np.float64)
-            for (index, row) in enumerate(rows):
-                col_data[col][index] = well_data[row + col]
-        return col_data
-
     # Process all sections.
     sections = dict()
     for (section_name, section_node) in section_nodes.iteritems():
         well_data = extract_well_data(section_node)
-        [rows, cols] = identify_rows_and_cols(well_data)
         sections[section_name] = dict()
         sections[section_name]['well_data'] = well_data
-        sections[section_name]['rows'] = extract_rows(well_data, rows, cols)
-        sections[section_name]['cols'] = extract_cols(well_data, rows, cols)
 
     return sections
+
+def identify_rows_and_cols(well_data):
+    """
+    Build a sorted list of unique rows and columns.
+
+    Parameters
+    ----------
+    well_data : dict
+       well_data[well_name] is the value associated with well_name (e.g. well_data['D7'] = 423)
+
+    Returns
+    -------
+    rows : list of str
+       Sorted list of unique rows.
+    cols : list of str
+       Sorted listof unique columns.
+
+    Examples
+    --------
+
+    >>> well_data = { 'A1' : 1, 'A2' : 2, 'A3' : 3, 'B1' : 4, 'B2' : 5, 'B3', 6 }
+    >>> [rows, cols] = identify_rows_and_cols(well_data)
+
+    """
+
+    rows = set()
+    cols = set()
+
+    for (well_name, value) in well_data.iteritems():
+        # Parse well names like 'A7' and 'D8'
+        m = re.match(r"([A-Z]+)([0-9]+)", well_name)
+
+        row = m.group(1)
+        col = int(m.group(2))
+
+        rows.add(row)
+        cols.add(col)
+
+    rows = sorted(list(rows))
+    cols = [ '%s' % entry for entry in sorted(list(cols)) ] # convert back to str after sorting numerically
+
+    return [rows, cols]
+
+def extract_rows(well_data, rows, cols):
+    """
+    Examples
+    --------
+
+    >>> well_data = { 'A1' : 1, 'A2' : 2, 'A3' : 3, 'B1' : 4, 'B2' : 5, 'B3', 6 }
+    >>> [rows, cols] = identify_rows_and_cols(well_data)
+    >>> row_data = extract_rows(well_data, rows, cols)
+
+    """
+
+    row_data = dict()
+    for row in rows:
+        row_data[row] = np.zeros([len(cols)], np.float64)
+        for (index, col) in enumerate(cols):
+            row_data[row][index] = well_data[row + col]
+    return row_data
+
+def extract_cols(well_data, rows, cols):
+    """
+    Examples
+    --------
+
+    >>> well_data = { 'A1' : 1, 'A2' : 2, 'A3' : 3, 'B1' : 4, 'B2' : 5, 'B3', 6 }
+    >>> [rows, cols] = identify_rows_and_cols(well_data)
+    >>> col_data = extract_cols(well_data, rows, cols)
+
+    """
+
+    col_data = dict()
+    for col in cols:
+        col_data[col] = np.zeros([len(rows)], np.float64)
+        for (index, row) in enumerate(rows):
+            col_data[col][index] = well_data[row + col]
+    return col_data
+
+def read_emission_spectra_text(filename):
+    """
+    Read text-formatted emission spectra.
+
+    Parameters
+    ----------
+    filename : str
+       The Tecan Infinite output filen to be read.
+
+    Returns
+    -------
+    SRC_280 : numpy.array
+    SRC_280_x : numpy.array
+    SRC_280_x_num : numpy.array
+
+    Examples
+    --------
+
+    """
+
+    SRC_280 = np.genfromtxt(filename, dtype='str')
+    SRC_280_x = SRC_280[0,:]
+    SRC_280_x_num = re.findall(r'\d+', str(SRC_280_x )[1:-1])
+
+    return [SRC_280, SRC_280_x, SRC_280_x_num]
