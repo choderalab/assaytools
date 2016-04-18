101f8591276bbc8c889a789cce00b912154b1e81
sonyahanson <hansons@mskcc.org>
2016-04-18 16:20:50 -0400

    select_data can now take dict as input

--- AssayTools/platereader.py
+++ AssayTools/platereader.py
@@ -94,14 +94,14 @@ def read_icontrol_xml(filename):
 
     return sections
 
-def select_data(filename,section_name,selection,*args,**kwargs):
+def select_data(file_data,section_name,selection,*args,**kwargs):
     """
     Read a Tecan iControl XML-formatted file and extract a particular part (row, column, 
     well, or well selection) for a particular section.
     Parameters
     ----------
-    filename : str
-       The name of the XML file to be read.
+    input : str or dict
+       EITHER the name of the XML file to be read OR the dict of an XML file made by read_icontrol_xml
     section_name : str
        The 'Name' attribute of the section to read.
     selection : str
@@ -132,8 +132,13 @@ def select_data(filename,section_name,selection,*args,**kwargs):
     for i in range(1,25):
         cols.append('%s' % i)
     
-    # import data from section
-    well_data = read_icontrol_xml(filename)
+    # read data from xml or dict
+    
+    if type(file_data) == dict:
+        well_data = file_data
+    else:
+        well_data = read_icontrol_xml(file_data)
+
     section_data = well_data[section_name]
     
     # extract selection
