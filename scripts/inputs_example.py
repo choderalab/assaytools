cb2480fb600b9663c40ff2ff777a15fd92111286
Sonya Hanson <hansons@gpu-2-4.local>
2017-05-03 17:18:37 -0400

    updated inputs_example

--- scripts/inputs_example.py
+++ scripts/inputs_example.py
@@ -1,11 +1,12 @@
 import json
 import numpy as np
+import os
 
 inputs = {
-    'xml_file_path' :  "/Users/hansons/Documents/github/fluorescence-assay-manuscript/data/singlet/DMSO-backfill/",
+    'xml_file_path' :  "%s/fluorescence-assay-manuscript/data/singlet/DMSO-backfill/"%os.environ['GITHUB_PATH'],
     'section'       :  '280_480_TOP_120',
     'ligand_order'  :  ['Bosutinib','Bosutinib Isomer','Erlotinib','Gefitinib','Bosutinib','Bosutinib Isomer','Erlotinib','Gefitinib'],
-    'Lstated'       :  np.array([20.0e-6,14.0e-6,9.82e-6,6.88e-6,4.82e-6,3.38e-6,2.37e-6,1.66e-6,1.16e-6,0.815e-6,0.571e-6,0.4e-6,0.28e-6,0.196e-6,0.138e-6,0.0964e-6,0.0676e-6,0.0474e-6,0.0320e-6,0.0240e-6,0.0160e-6,0.0120e-6,0.008e-6,0.00001e-6], np.float64), # ligand concentration, M
+    'Lstated'       :  np.array([20.0e-6,14.0e-6,9.82e-6,6.88e-6,4.82e-6,3.38e-6,2.37e-6,1.66e-6,1.16e-6,0.815e-6,0.571e-6,0.4e-6,0.28e-6,0.196e-6,0.138e-6,0.0964e-6,0.0676e-6,0.0474e-6,0.0320e-6,0.0240e-6,0.0160e-6,0.0120e-6,0.008e-6,0.0], np.float64), # ligand concentration, M
     'protein'       :  'p38',
     'Pstated'       :  0.5e-6 * np.ones([24],np.float64), # protein concentration, M
     'assay_volume'  :  50e-6, # assay volume, L
